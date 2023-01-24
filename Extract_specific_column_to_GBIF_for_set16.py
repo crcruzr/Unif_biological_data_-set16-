5a62951fc0ca272b63c473aed1385743a674c553
crcruzr <cristian.cruz@humboldt.local>
2023-01-24 17:32:48 -0500

    Script to extract only columns used in BioModelos analisys

--- Extract_specific_column_to_GBIF_for_set16.py
+++ Extract_specific_column_to_GBIF_for_set16.py
@@ -3,6 +3,8 @@
 Created on Wed Jul 22 11:11:41 2020
 
 @author: cristian.cruz
+
+This scrict reduces the number of columns in GBIF data so that it can be used in R
 """
 
 # Extract data GBIF to set16 format
@@ -11,10 +13,10 @@ import os
 
 print(os.getcwd()) ##identify the folder directory
 
-#os.chdir('G:/Cristian data/Humboldt/Scripts/Python/extract_specific_column_to_GBIF_for_set16.py') ##folder for the code
+#os.chdir('G:/Cristian_data/Humboldt/Git/Unif_biological_data_-set16-/extract_specific_column_to_GBIF_for_set16.py') ##folder for the code
 
 #Set16 Folder
-os.chdir('D:/Set16/datos_originales/GBIF&SiB/raw/Descarga_2022_01_13_directGBIF/0112920-210914110416597/')
+os.chdir('D:/Set16/datos_originales/GBIF&SiB/raw/Descarga_2023_01_23_directGBIF/0255995-220831081235567/')
                       
 
 print(os.getcwd())
@@ -30,4 +32,4 @@ print(gbib_cn)
 df = pd.read_csv('occurrence.txt', header = 0, sep='\t', skipinitialspace=False, usecols=columns, low_memory=True, encoding='utf-8')
 
 #export
-df.to_csv('filteredOcc2.1.txt', header=True, index=None, sep=';', encoding='utf-8')
+df.to_csv('filteredOcc.txt', header=True, index=None, sep=';', encoding='utf-8')
