3f417177bec9af7ba787bb1f22bd3b97d4dfcd1e
crcruzr <cristian.cruz@humboldt.local>
2023-01-24 17:27:44 -0500

    Script to extract only columns used in BioModelos analisys

---
+++ Extract_specific_column_to_GBIF_for_set16.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 22 11:11:41 2020
+
+@author: cristian.cruz
+"""
+
+# Extract data GBIF to set16 format
+import pandas as pd
+import os
+
+print(os.getcwd()) ##identify the folder directory
+
+#os.chdir('G:/Cristian data/Humboldt/Scripts/Python/extract_specific_column_to_GBIF_for_set16.py') ##folder for the code
+
+#Set16 Folder
+os.chdir('D:/Set16/datos_originales/GBIF&SiB/raw/Descarga_2022_01_13_directGBIF/0112920-210914110416597/')
+                      
+
+print(os.getcwd())
+gbib_cn = pd.read_csv('occurrence.txt', nrows=1, sep='\t').columns.tolist()
+
+print(gbib_cn)
+
+columns=[ 'gbifID', 'accessRights', 'institutionCode', 'collectionCode', 'basisOfRecord', 'occurrenceID', 'catalogNumber', 'recordedBy', 'eventDate', 'year', 'month', 'day', 'continent', 'countryCode','stateProvince', 'county', 'municipality', 'locality', 'decimalLatitude', 'decimalLongitude', 'coordinateUncertaintyInMeters', 'scientificName', 'elevation', 'kingdom', 'phylum', 'class', 'order', 'family',  'genus', 'specificEpithet', 'scientificName']
+
+print(columns)
+print(gbib_cn)
+
+df = pd.read_csv('occurrence.txt', header = 0, sep='\t', skipinitialspace=False, usecols=columns, low_memory=True, encoding='utf-8')
+
+#export
+df.to_csv('filteredOcc2.1.txt', header=True, index=None, sep=';', encoding='utf-8')
